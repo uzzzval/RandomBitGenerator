028a2c5b64a69421126ea45a382832026bcfa69a
Ujjawal Sharma <uzzzval@gmail.com>
2018-01-26 21:31:57 -0500

    Update RandomBitGenerator.py
    Removing print statements.

--- RandomBitGenerator.py
+++ RandomBitGenerator.py
@@ -10,7 +10,6 @@ timeString=timeString.replace(":","")
 timeString=timeString.replace(".","")
 timeString=timeString.replace(" ","")
 
-
 binaryNumber=bin(int(binascii.hexlify(timeString),16))
 binaryNumber = binaryNumber[2:]
 randomLength=len(binaryNumber)
@@ -19,8 +18,6 @@ padString=""
 for x in range(1, randomLength):
     randomNumber=random.choice(randomNumbers)
     padString=padString+str(randomNumber)
-
-print padString
 
 finalString = int(binaryNumber,2) ^ int(binaryNumber,2)
 finalString = str(finalString)[2:]
